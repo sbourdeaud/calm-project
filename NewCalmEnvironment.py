7d352f4b9642448d270ca9c6563795508bad053a
bogdannm23 <bogdan19_bigm@yahoo.com>
2019-09-17 14:09:30 +0200

    changed -add regions to script

--- NewCalmEnvironment.py
+++ NewCalmEnvironment.py
@@ -1,12 +1,78 @@
+# region headers
+# escript-template v20190611 / stephane.bourdeaud@nutanix.com
+# * author:     MITU Bogdan Nicolae (EEAS-EXT) <Bogdan-Nicolae.MITU@ext.eeas.europa.eu>
+# * version:    2019/09/17
+# task_name:    NewCalmEnvironment
+# description:  Create enviroment configuration for Calm. Environment is mandatory to publish the applications into the marketplace. In case while creating a blueprint the VM configuration is not defined then the configuration needs to be defined as part of environment. Also, during the application blueprint launch from marketplace the values are picked from environment. Only one environment per project is applicable in case of different marketplace application blueprints have different VM requirements.
+# output vars:  nameuuid,  imageuuid (environment name and environment uuid)
+# endregion
+
+#region capture Calm variables
+username = "@@{pc.username}@@"
+username_secret = "@@{pc.secret}@@"
+api_server = "@@{pc_ip}@@"
+project_name = "@@{Project_Name}@@"
+project_uuid = "@@{Project_UUID}@@"
+project_vlan_id = "@@{project_vlan_id}@@"
+ahv_network_uuid = "@@{ahv_network_uuid}@@"
+ahv_network_name = "@@{ahv_network_name}@@"
+env_uuid = "@@{envuuid}@@"
+os_user= "@@{local1.username}@@"
+os_secret= "@@{local1.secret}@@"
+rand_num = "@@{calm_unique}@@"
+Image_Name = "@@{Image_Name}@@"
+#TODO is this still neede it?  Image_Name = "@@{Image_Name}@@"
+vm_name = "vml-@" + my_var + "@"+"rand_num"
+
+#TODO move var up here?  envuuid = str(uuid.uuid4())
+#TODO move var up here? nameuuid = str(uuid.uuid4())
+#TODO move var up here? my_var1="@{platform.status.resources.nic_list[0].ip_endpoint_list[0].ip}@"
+# endregion
+
+#region prepare api call
+api_server_port = "9440"
+api_server_endpoint = "/api/nutanix/v3/images/list"
+length = 100
+url = "https://{}:{}{}".format(
+    api_server,
+    api_server_port,
+    api_server_endpoint
+)
+method = "POST"
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
+
+# Compose the json payload
+payload = {
+    "filter":"name==@@{Image_Name}@@", 
+    "length": 100, "offset": 0
+}
+#TODO should we use image_name as a filter?
+# endregion
+
+#region make the api call
+
+# endregion
+
+
+#region process the results
+
+# endregion
+
+
+#! Old Script moved to GetImageUuid.py
+
 url     = "https://@@{PC_IP}@@:9440/api/nutanix/v3/images/list"
 headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
-payload = {"filter":"name==@@{Image_Name}@@", "length": 100, "offset": 0,}
+payload = {"filter":"name==@@{Image_Name}@@", "length": 100, "offset": 0}
 
-pc_user =  '@@{Prism_Central.username}@@'
-pc_pass = '@@{Prism_Central.secret}@@'
+username =  '@@{pc.username}@@'
+username_secret = '@@{pc.secret}@@'
 
-resp = urlreq(url, verb='POST', auth='BASIC', user=pc_user, passwd=pc_pass, params=json.dumps(payload), headers=headers)
+resp = urlreq(url, verb='POST', auth='BASIC', user=username, passwd=username_secret, params=json.dumps(payload), headers=headers)
 
 if resp.ok:
   print json.dumps(json.loads(resp.content), indent=4)
@@ -18,12 +84,14 @@ else:
   
 print imageuuid
 
+#! Old script
+
 url     = "https://@@{PC_IP}@@:9440/api/nutanix/v3/environments"
 headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
 envuuid = str(uuid.uuid4())
 nameuuid = str(uuid.uuid4())
-my_var= "@{Project_Name}@"
+project_name= "@{Project_Name}@"
 my_var1="@{platform.status.resources.nic_list[0].ip_endpoint_list[0].ip}@"
 
 
@@ -64,7 +132,7 @@ payload = {
               "nic_list": [
                 {
                   "subnet_reference": {
-                    "uuid": "@@{Subnet_UUID}@@"
+                    "uuid": "ahv_network_uuid"
                   },
                   "ip_endpoint_list": []
                 }
@@ -97,7 +165,7 @@ payload = {
                 }
               ]
             },
-            "name": "vml-@" + my_var + "@"+"@@{calm_unique}@@",
+            "name": "vm_name",
             "categories": {}
           },
           "variable_list": [],
@@ -108,12 +176,12 @@ payload = {
         {
           "name": "root",
           "type": "PASSWORD",
-          "username": "@@{local.username}@@",
+          "username": "os_user",
           "secret": {
             "attrs": {
               "is_secret_modified": "true"
             },
-            "value": "@@{local.secret}@@"
+            "value": "os_secret"
           },
           "uuid": nameuuid
         }
@@ -128,11 +196,11 @@ payload = {
 }
 
 
-pc_user =  '@@{Prism_Central.username}@@'
-pc_pass = '@@{Prism_Central.secret}@@'
+username =  '@@{pc.username}@@'
+username_secret = '@@{pc.secret}@@'
 
 
-resp = urlreq(url, verb='POST', auth='BASIC', user=pc_user, passwd=pc_pass, params=json.dumps(payload), headers=headers)
+resp = urlreq(url, verb='POST', auth='BASIC', user=username, passwd=username_secret, params=json.dumps(payload), headers=headers)
 
 if resp.ok:
   print json.dumps(json.loads(resp.content), indent=4)
