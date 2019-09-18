13bfc7ed43244e8593beddcaaa644862ef04bc73
Stephane Bourdeaud <stephanebourdeaud@hotmail.com>
2019-09-18 16:38:55 +0200

    Removed GetPcDirSvcUuid.py. Fixed an issue with GetAdUSerUuid

--- GetAdUserUuid.py
+++ GetAdUserUuid.py
@@ -86,7 +86,7 @@ if resp.ok:
             #TODO: see if user matches here
             for directory_user in json_resp['entities']:
                 if nutanix_calm_user_upn == directory_user['status']['name']:
-                    calm_user_uuid = directory_user['metadata']['uuid']
+                    nutanix_calm_user_uuid = directory_user['metadata']['uuid']
                     directory_uuid = directory_user['spec']['resources']['directory_service_user']['directory_service_reference']['uuid']
                     print("nutanix_calm_user_uuid={}".format(nutanix_calm_user_uuid))
                     print("directory_uuid={}".format(directory_uuid))
