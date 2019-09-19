ea39b2788ab0aa7c66418f8da16bc4a93feeeaaa
Stephane Bourdeaud <stephanebourdeaud@hotmail.com>
2019-09-19 10:07:19 +0200

    enhance

--- SetCalmProjectBp.py
+++ SetCalmProjectBp.py
@@ -52,7 +52,7 @@ resp = urlreq(
 # endregion
 #region process the results (get marketplace items)
 if resp.ok:
-    print ("Request status code {} on {}".format(resp.status_code,url))
+    print ("Request status code {} on {}".format(resp.status_code,resp.request.url))
     json_resp = json.loads(resp.content)
     marketplace_items = json_resp['entities']
     #process each marketplace item
@@ -81,7 +81,7 @@ if resp.ok:
         #endregion
         #region process results (get marketplace information)
         if resp.ok:
-            print ("Request status code {} on {}".format(resp.status_code,url))
+            print ("Request status code {} on {}".format(resp.status_code,resp.request.url))
             json_resp = json.loads(resp.content)
             json_resp['metadata'].pop('owner_reference', None)
             json_resp.pop('status', None)
@@ -111,7 +111,7 @@ if resp.ok:
             #endregion
             #region process results (publish marketplace item to project)
             if resp.ok:
-                print ("Request status code {} on {}".format(resp.status_code,url))
+                print ("Request status code {} on {}".format(resp.status_code,resp.request.url))
                 print "Marketplace item with uuid {} is published.".format(marketplace_item_uuid)
             else:
                 print ("Request failed with status code {}".format(resp.status_code))
